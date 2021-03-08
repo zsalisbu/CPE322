e103d69e679a09d94051a56c0cbcdaf8a845f060
zsalisbu <68284896+zsalisbu@users.noreply.github.com>
2021-03-07 21:38:42 -0500

    Delete time_example.py

--- time_example.py
+++
@@ -1,8 +0,0 @@
-import time
-while True:
-    try:
-        nowtime = time.time()
-        print(time.asctime(time.localtime(nowtime)))
-        time.sleep(10)
-    except KeyboardInterrupt:
-        exit()
